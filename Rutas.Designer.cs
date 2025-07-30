95c8434c7adaac4537d52fa93b983a649d77fe17
Rocio López <guisell.alvarado@gmail.com>
2025-07-30 16:53:08 -0600

    creacion de formulario Rutas

---
+++ Rutas.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace SistemaRepartoG4
+{
+    partial class Rutas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Rutas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1215, 703);
+            this.Name = "Rutas";
+            this.Text = "Rutas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
