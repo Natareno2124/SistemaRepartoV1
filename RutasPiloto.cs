ae5d273c8e6fd1e224b7aad3e7e093170e6c0540
MARIA ALEJANDRA MORALES GARCIA <mmoralesg42@miumg.edu.gt>
2025-08-02 19:20:24 -0600

    Modificando Menu Principal, redimencion de botones y direccionamiento. Agregado de botones en mis entregas y Rutas Piloto

---
+++ RutasPiloto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaRepartoG4
+{
+    public partial class RutasPiloto : Form
+    {
+        public RutasPiloto()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            MenuPiloto ventanaMenuPiloto = new MenuPiloto();
+            ventanaMenuPiloto.FormClosed += (s, args) => this.Show();
+            ventanaMenuPiloto.Show();
+            this.Hide();
+        }
+    }
+}
