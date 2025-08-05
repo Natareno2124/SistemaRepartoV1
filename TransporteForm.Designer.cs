a49b9d901ce6426ff70ff48e00fd41f8886ef361
Vennus <vennussalguero@gmail.com>
2025-08-05 08:26:31 -0600

    Update TransporteForm.Designer.cs
    Modulo transporte searh

--- TransporteForm.Designer.cs
+++ TransporteForm.Designer.cs
@@ -407,9 +407,6 @@ namespace SistemaRepartoG4
             //llamamos al crud y a obtener transporte
             TransporteCRUD crud = new TransporteCRUD();
 
-
-            // Dentro de tu constructor o método de inicialización, tras InitializeComponent():
-
             // Funcionalidad de buscar por placa
             DataTable dt = new TransporteCRUD().ObtenerTransportes();
             transportesBinding = new BindingSource();
@@ -433,7 +430,6 @@ namespace SistemaRepartoG4
                 }
             };
 
-            // Placeholder: al enfocar quita el texto por defecto
             txtBuscar.GotFocus += (s, e) =>
             {
                 if (txtBuscar.Text == "Buscar por placa")
@@ -442,8 +438,6 @@ namespace SistemaRepartoG4
                     txtBuscar.ForeColor = SystemColors.WindowText;
                 }
             };
-
-            // Placeholder: al perder foco, restaura si está vacío
             txtBuscar.LostFocus += (s, e) =>
             {
                 if (string.IsNullOrWhiteSpace(txtBuscar.Text))
@@ -453,10 +447,6 @@ namespace SistemaRepartoG4
                     transportesBinding.RemoveFilter();
                 }
             };
-
-
-
-
 
             //centrar los datos de la tabla 
             foreach (DataGridViewColumn col in dgvTransporte.Columns)
