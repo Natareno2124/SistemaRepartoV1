89caaa93bd8f28f4549347cc0211b695944f7b7a
MARIA ALEJANDRA MORALES GARCIA <mmoralesg42@miumg.edu.gt>
2025-08-05 17:07:30 -0600

    Se agrego el modulo de PAQUETES y se agregaron los formularios de editar, guardar y eliminar

---
+++ Clases/Paquete.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaRepartoG4.Clases
+{
+    public class Paquete
+    {
+        public int IdGuiaMadre { get; set; }
+        public float PesoKg { get; set; }
+        public float LargoCm { get; set; }
+        public float AnchoCm { get; set; }
+        public float AltoCm { get; set; }
+        public string Descripcion { get; set; }
+        public float Precio { get; set; }
+        public string Fragil { get; set; } // aqui colocar S si es si, una N si es no 
+        public int TiempoEntrega { get; set; }
+        public int IdBodega { get; set; } // esto ya debe estar en la BD porque llama a la tabla de bodega
+        public int IdSucursal { get; set; } // esto ya debe estar en la BD porque llama a la tabla de sucursal
+    }
+}
---
+++ EliminarPaquete.cs
@@ -0,0 +1,50 @@
+﻿using SistemaRepartoG4.Clases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaRepartoG4
+{
+    public partial class EliminarPaquete : Form
+    {
+        public EliminarPaquete()
+        {
+            InitializeComponent();
+        }
+
+        private void EliminarPaquete_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_eliminar_p_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(id_guia_madre.Text))
+                {
+                    MessageBox.Show("Por favor ingresa el ID de guía madre.");
+                    return;
+                }
+
+                int id = int.Parse(id_guia_madre.Text);
+
+                bool eliminado = PaquetesCRUD.EliminarPaquete(id);
+                if (eliminado)
+                {
+                    id_guia_madre.Clear();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
