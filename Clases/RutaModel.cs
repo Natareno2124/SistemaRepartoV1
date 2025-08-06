cd6d241ca6c9821da1cb8fdb5e77434b811a1327
Rocio López <guisell.alvarado@gmail.com>
2025-08-05 18:18:45 -0600

    Recreando archivos RegistroRutas, RutaModel para base de datos, y ValidarRutas

---
+++ Clases/RutaModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaRepartoG4
+{
+    public class RutaModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Fecha { get; set; }
+        public string HoraInicio { get; set; }
+        public string HoraFinal { get; set; }
+        public int IdConductor { get; set; }
+    }
+}
---
+++ RegistroRutas.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace SistemaRepartoG4
+{
+    partial class RegistroRutas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_hora_final_ruta = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Nombre = new System.Windows.Forms.Label();
+            this.txt_Id_Ruta = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_id_conductor = new System.Windows.Forms.TextBox();
+            this.txt_Nombre_de_Ruta = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_hora_inicio_ruta = new System.Windows.Forms.TextBox();
+            this.txtFecha_Ruta = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_registra = new System.Windows.Forms.Button();
+            this.cmb_id_conductor = new System.Windows.Forms.ComboBox();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(67)))), ((int)(((byte)(88)))));
+            this.panel2.Controls.Add(this.groupBox1);
+            this.panel2.Controls.Add(this.btn_registra);
+            this.panel2.Location = new System.Drawing.Point(12, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1139, 658);
+            this.panel2.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(153)))), ((int)(((byte)(174)))));
+            this.groupBox1.Controls.Add(this.cmb_id_conductor);
+            this.groupBox1.Controls.Add(this.txt_hora_final_ruta);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.Nombre);
+            this.groupBox1.Controls.Add(this.txt_Id_Ruta);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txt_id_conductor);
+            this.groupBox1.Controls.Add(this.txt_Nombre_de_Ruta);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txt_hora_inicio_ruta);
+            this.groupBox1.Controls.Add(this.txtFecha_Ruta);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.ForeColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Location = new System.Drawing.Point(34, 28);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(1047, 392);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            // 
+            // txt_hora_final_ruta
+            // 
+            this.txt_hora_final_ruta.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(84)))), ((int)(((byte)(109)))));
+            this.txt_hora_final_ruta.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_hora_final_ruta.Font = new System.Drawing.Font("Arial", 16.2F);
+            this.txt_hora_final_ruta.Location = new System.Drawing.Point(594, 169);
+            this.txt_hora_final_ruta.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_hora_final_ruta.Name = "txt_hora_final_ruta";
+            this.txt_hora_final_ruta.PasswordChar = '*';
+            this.txt_hora_final_ruta.Size = new System.Drawing.Size(383, 32);
+            this.txt_hora_final_ruta.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold);
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(52)))), ((int)(((byte)(70)))));
+            this.label7.Location = new System.Drawing.Point(594, 148);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(119, 19);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "hora final ruta";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold);
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(52)))), ((int)(((byte)(70)))));
+            this.label6.Location = new System.Drawing.Point(42, 36);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(211, 28);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "REGISTRO RUTAS";
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSize = true;
+            this.Nombre.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold);
+            this.Nombre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(52)))), ((int)(((byte)(70)))));
+            this.Nombre.Location = new System.Drawing.Point(73, 86);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(65, 19);
+            this.Nombre.TabIndex = 1;
+            this.Nombre.Text = "Id Ruta";
+            // 
+            // txt_Id_Ruta
+            // 
+            this.txt_Id_Ruta.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(84)))), ((int)(((byte)(109)))));
+            this.txt_Id_Ruta.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_Id_Ruta.Font = new System.Drawing.Font("Arial", 16.2F);
+            this.txt_Id_Ruta.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.txt_Id_Ruta.Location = new System.Drawing.Point(73, 107);
+            this.txt_Id_Ruta.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_Id_Ruta.Name = "txt_Id_Ruta";
+            this.txt_Id_Ruta.Size = new System.Drawing.Size(383, 32);
+            this.txt_Id_Ruta.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(52)))), ((int)(((byte)(70)))));
+            this.label1.Location = new System.Drawing.Point(73, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nombre de Ruta";
+            // 
+            // txt_id_conductor
+            // 
+            this.txt_id_conductor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(84)))), ((int)(((byte)(109)))));
+            this.txt_id_conductor.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_id_conductor.Font = new System.Drawing.Font("Arial", 16.2F);
+            this.txt_id_conductor.Location = new System.Drawing.Point(585, 288);
+            this.txt_id_conductor.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_id_conductor.Name = "txt_id_conductor";
+            this.txt_id_conductor.Size = new System.Drawing.Size(383, 32);
+            this.txt_id_conductor.TabIndex = 10;
+            // 
+            // txt_Nombre_de_Ruta
+            // 
+            this.txt_Nombre_de_Ruta.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(84)))), ((int)(((byte)(109)))));
+            this.txt_Nombre_de_Ruta.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_Nombre_de_Ruta.Font = new System.Drawing.Font("Arial", 16.2F);
+            this.txt_Nombre_de_Ruta.Location = new System.Drawing.Point(73, 169);
+            this.txt_Nombre_de_Ruta.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_Nombre_de_Ruta.Name = "txt_Nombre_de_Ruta";
+            this.txt_Nombre_de_Ruta.Size = new System.Drawing.Size(383, 32);
+            this.txt_Nombre_de_Ruta.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(52)))), ((int)(((byte)(70)))));
+            this.label4.Location = new System.Drawing.Point(594, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 19);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "id conductor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(52)))), ((int)(((byte)(70)))));
+            this.label2.Location = new System.Drawing.Point(73, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Fecha Ruta";
+            // 
+            // txt_hora_inicio_ruta
+            // 
+            this.txt_hora_inicio_ruta.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(84)))), ((int)(((byte)(109)))));
+            this.txt_hora_inicio_ruta.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_hora_inicio_ruta.Font = new System.Drawing.Font("Arial", 16.2F);
+            this.txt_hora_inicio_ruta.Location = new System.Drawing.Point(594, 108);
+            this.txt_hora_inicio_ruta.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_hora_inicio_ruta.Name = "txt_hora_inicio_ruta";
+            this.txt_hora_inicio_ruta.PasswordChar = '*';
+            this.txt_hora_inicio_ruta.Size = new System.Drawing.Size(383, 32);
+            this.txt_hora_inicio_ruta.TabIndex = 8;
+            // 
+            // txtFecha_Ruta
+            // 
+            this.txtFecha_Ruta.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(84)))), ((int)(((byte)(109)))));
+            this.txtFecha_Ruta.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtFecha_Ruta.Font = new System.Drawing.Font("Arial", 16.2F);
+            this.txtFecha_Ruta.Location = new System.Drawing.Point(73, 230);
+            this.txtFecha_Ruta.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtFecha_Ruta.Name = "txtFecha_Ruta";
+            this.txtFecha_Ruta.Size = new System.Drawing.Size(383, 32);
+            this.txtFecha_Ruta.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(52)))), ((int)(((byte)(70)))));
+            this.label3.Location = new System.Drawing.Point(594, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 19);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Hora inicio ruta";
+            // 
+            // btn_registra
+            // 
+            this.btn_registra.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(153)))), ((int)(((byte)(174)))));
+            this.btn_registra.Font = new System.Drawing.Font("Arial Black", 10.8F, System.Drawing.FontStyle.Bold);
+            this.btn_registra.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_registra.Location = new System.Drawing.Point(449, 447);
+            this.btn_registra.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_registra.Name = "btn_registra";
+            this.btn_registra.Size = new System.Drawing.Size(264, 55);
+            this.btn_registra.TabIndex = 0;
+            this.btn_registra.Text = "REGISTRAR";
+            this.btn_registra.UseVisualStyleBackColor = false;
+            this.btn_registra.Click += new System.EventHandler(this.btn_guardar);
+            // 
+            // cmb_id_conductor
+            // 
+            this.cmb_id_conductor.FormattingEnabled = true;
+            this.cmb_id_conductor.Location = new System.Drawing.Point(594, 231);
+            this.cmb_id_conductor.Name = "cmb_id_conductor";
+            this.cmb_id_conductor.Size = new System.Drawing.Size(176, 24);
+            this.cmb_id_conductor.TabIndex = 16;
+            this.cmb_id_conductor.SelectedIndexChanged += new System.EventHandler(this.cmb_id_conductor_SelectedIndexChanged);
+            // 
+            // RegistroRutas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1177, 775);
+            this.Controls.Add(this.panel2);
+            this.Name = "RegistroRutas";
+            this.Text = "RegistroRutas";
+            this.panel2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_hora_final_ruta;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label Nombre;
+        private System.Windows.Forms.TextBox txt_Id_Ruta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_id_conductor;
+        private System.Windows.Forms.TextBox txt_Nombre_de_Ruta;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_hora_inicio_ruta;
+        private System.Windows.Forms.TextBox txtFecha_Ruta;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_registra;
+        private System.Windows.Forms.ComboBox cmb_id_conductor;
+    }
+}
